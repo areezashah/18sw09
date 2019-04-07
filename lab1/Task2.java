1bbb735cbbea8d6304ecbeb3d36e2d3a4bcac0a7
areezashah <49343561+areezashah@users.noreply.github.com>
2019-04-07 19:30:54 +0500

    Add files via upload

---
+++ lab1/Task2.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class Task2{
+public static void main(String []args){
+double a;
+a=50.25;
+long b=(long)a;
+double c=a-b;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+}
+}
---
+++ lab1/task1.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+class Task1{
+public static void main(String []args){
+
+String name;
+name="areeza";
+System.out.println("Name : " + name);
+
+int obtainedmarks;
+obtainedmarks=100;
+System.out.println("OBTAINED MARKS : " + obtainedmarks);
+
+byte totalmarks;
+totalmarks=80;
+System.out.println("TOTAL MARKS : " + totalmarks);
+
+float per;
+per=80f;
+System.out.println("PERCENTAGE : " + per + "%");
+
+char grade;
+grade='A';
+System.out.println("GRADE : " + grade );
+
+}
+}
